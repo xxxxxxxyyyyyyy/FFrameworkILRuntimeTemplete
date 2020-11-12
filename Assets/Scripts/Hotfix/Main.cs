d32b67221ffa3d4acedb655fb37b661ecdcbfb74
fujisheng <fujisheng@foxmail.com>
2020-11-12 11:01:58 +0800

    测试元组

--- Assets/Scripts/Hotfix/Main.cs
+++ Assets/Scripts/Hotfix/Main.cs
@@ -10,6 +10,14 @@ namespace Game.Hotfix
             Debug.Log($"aaaaa{Layer.POPUP}");
             Debug.Log("hello ilruntime");
             Debug.Log("你在干啥呢");
+            Debug.Log(Get().Item1);
+            Debug.Log(Get().Item2);
+            Debug.Log(Get().Item3);
+        }
+
+        static (int, string, bool) Get()
+        {
+            return (1, "2", true);
         }
     }
 }
